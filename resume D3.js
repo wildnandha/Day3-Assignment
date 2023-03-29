02733341b679636a92f810c01603928aa79af151
NANDHAGOPAL <100506837+wildnandha@users.noreply.github.com>
2023-03-29 21:56:25 +0530

    Add files via upload

---
+++ resume D3.js
@@ -0,0 +1,11 @@
+const resume = [
+    {
+    "Name":"Nandhagopal",
+    "Education":"M.sc"},
+    {
+     "Department":"Geoinformatics",
+     "Experience":"4 Years",
+     "Company":"Trimble Maps"
+}   
+]
+console.log (resume)
